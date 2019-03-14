fixed a critical bug on ppo clamping. Changed the initial std scaling value. changed the last layer of critic from 32 to 64. So far this seems stable. reached 8 at around episode 250.

PPO_agent.py
@@ -43,8 +43,8 @@ CRITIC_L_WEIGHT = 0.5         # mean square error term weight
 ENT_WEIGHT = 0.02             # weight of entropy added
 ENT_DECAY = 0.999             # decay of entropy per 'step'
 ENT_MIN = 1e-3                # min weight of entropy
-STD_SCALE_INIT = 1.0          # initial value of std scale for action resampling
-STD_SCALE_DECAY = 0.99        # scale decay of std
+STD_SCALE_INIT = 2.0          # initial value of std scale for action resampling
+STD_SCALE_DECAY = 0.999       # scale decay of std
 STD_SCALE_MIN = 0.01          # min value of STD scale
 LEARNING_LOOP = 4             # no of update on grad per step
 P_RATIO_EPS = 0.2             # eps for ratio clip 1+eps, 1-eps
@@ -243,8 +243,7 @@ class PPO_Agent():
             # std decay
             self.std_scale = max(self.std_scale * STD_SCALE_DECAY, STD_SCALE_MIN)
 
-            self.t_step += 1
-
+        self.t_step += 1
 
     def learn(self, m_batch):
         """Update the parameters of the policy based on the data in the sampled
@@ -273,7 +272,7 @@ class PPO_Agent():
 
         G = ratio * A
 
-        G_clipped = torch.clamp(ratio, 1.+P_RATIO_EPS, 1.-P_RATIO_EPS) * A
+        G_clipped = torch.clamp(ratio, 1.-P_RATIO_EPS, 1.+P_RATIO_EPS) * A
 
         G_loss = torch.min(G, G_clipped).mean()
 

PPO_models2.py
@@ -78,7 +78,7 @@ class PPO_Actor(nn.Module):
 
         # base on the action as mean create a distribution with zero std...
         #dist = torch.distributions.Normal(a_mean, F.softplus(self.std))
-        dist = torch.distributions.Normal(a_mean, F.hardtanh(self.std, min_val=0.02*std_scale, max_val=0.25*std_scale))
+        dist = torch.distributions.Normal(a_mean, F.hardtanh(self.std, min_val=0.02*std_scale, max_val=0.5*std_scale))
 
         # sample from the prob distribution just generated again
         if resampled_action is None:
@@ -127,6 +127,8 @@ class PPO_Critic(nn.Module):
         self.fc_1m = nn.Linear(hidden_layer1+action_size, hidden_layer2, bias=True)
         self.bn_2m = nn.BatchNorm1d(hidden_layer2)
 
+
+
         self.fc_2m = nn.Linear(hidden_layer2, hidden_layer3, bias=True)
         self.bn_3m = nn.BatchNorm1d(hidden_layer3)
 
@@ -169,7 +171,7 @@ class PPO_ActorCritic(nn.Module):
         super(PPO_ActorCritic, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.actor = PPO_Actor(state_size, action_size, device, 512, 128, 64, seed=seed)
-        self.critic = PPO_Critic(state_size, action_size, device, 512, 64, 32, seed=seed)
+        self.critic = PPO_Critic(state_size, action_size, device, 512, 64, 64, seed=seed)
 
 
     def forward(self, s, action=None, std_scale=1.0):